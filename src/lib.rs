b2f5a24b3c298d6c9ba8f66aee6131d536ac104c
Luciano Bestia <LucianoBestia@gmail.com>
2021-10-14 08:17:53 +0200

    init

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+// dev_bestia_html_templating lib.rs
+
+// region: auto_md_to_doc_comments include README.md A //!
+
+// endregion: auto_md_to_doc_comments include README.md A //!
+
+pub mod html_template_mod;
+mod macros_mod;
+mod url_utf8_mod;
+mod utils_mod;
